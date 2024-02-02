ec739e19ac1ac3377c4ad43eefc83015379f4c9c
SviatoslavPaseka <paseka.syat@gmail.com>
2024-02-02 16:02:52 +0200

    udpated condition of sending notification to slack

--- test_and_notification.sh
+++ test_and_notification.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
 
+# Function to display example of using
+display_usage() {
+    echo "Usage: $0 <run_scala>"
+    echo "Example: $0 true"
+    echo "    or: $0 false"
+    exit 1
+}
+
+# Check if at least one argument is provided
+if [ $# -ne 1 ]; then
+    display_usage
+fi
+
 # Run Gatling test
 mvn gatling:test -Dgatling.simulationClass=com.eshop.qa.simulation.NewParameterizedSimulation
 
-# Run Scala application
-mvn scala:run -DmainClass=com.eshop.qa.utils.SlackNotificationMessenger
+# Check the value of the parameter
+if [ "$1" == "true" ]; then
+    # Run Scala application
+    mvn scala:run -DmainClass=com.eshop.qa.utils.SlackNotificationMessenger
+elif [ "$1" != "false" ]; then
+    echo "Invalid argument. Please use 'true' or 'false'."
+    display_usage
+fi
